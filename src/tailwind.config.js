8a877ee92032453bb7a3aa232047edf3b638fc88
Nikita Yeldynov <yeldynov@gmail.com>
2024-06-28 16:37:03 +0000

    + navbar

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        brand: '#FF6849',
+      },
+    },
+  },
   content: ["./**/*.{html,js,mjs,md,cshtml,razor,cs}","./Pages/**/*.{cshtml,razor}"],
   darkMode: 'class',
   plugins: [],
